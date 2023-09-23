b571e9698dbf1a24ec0f55c996cb0834cf6b4c59
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-09-23 08:15:14 -0500

    Create project, arquitecture and register user db

---
+++ src/main/java/com/inn/cafe/CafeMAnagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMAnagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CafeMAnagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inn/cafe/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.inn.cafe.service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface IUserService {
+
+    ResponseEntity<String> signUp(Map<String, String> requestMap);
+}
