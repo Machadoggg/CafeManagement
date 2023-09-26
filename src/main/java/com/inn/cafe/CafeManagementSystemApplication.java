9602aadfe29998eae973ce118272d4ed35981ae9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-09-26 00:15:59 -0500

    Adjust name java file

--- src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
+++ src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CafeMAnagementSystemApplication {
+public class CafeManagementSystemApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(CafeMAnagementSystemApplication.class, args);
+		SpringApplication.run(CafeManagementSystemApplication.class, args);
 	}
 
 }
